0d88a376a4c5c1e599f6c20b6e94cdc529b5826e
hanniaidn <113195557+hanniaidn@users.noreply.github.com>
2022-11-10 14:50:54 -0600

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,28 @@
 
+//
+// Created by Hannia Domínguez on 11/8/2022.
+//
+
+#ifndef PUNTO_PUNTO_H
+#define PUNTO_PUNTO_H
+#include <vector>
+
+using namespace std;
+
+class Punto2D {
+public:
+    Punto2D();
+    void SetPosicion(float x, float y);
+    void Trasladar(float t1, float t2);
+    void RotarRespectoAlOrigen(float t1);
+    void Escalar(float t1, float t2);
+    float GetX();
+    float GetY();
+
+private:
+    float a, b, c;
+};
+
+
+
+#endif //PUNTO_PUNTO_H
