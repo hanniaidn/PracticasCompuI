ddefe438dd4047711b833f35e459d75e1006fd35
hanniaidn <113195557+hanniaidn@users.noreply.github.com>
2022-11-23 19:00:10 -0600

    Update Grupo.h

--- CargaElectrica/Grupo.h
+++ CargaElectrica/Grupo.h
@@ -1,1 +1,32 @@
+//
+// Created by Hannia Domínguez on 23/11/2022.
+//
 
+#ifndef CARGAELECTRICA_GRUPO_H
+#define CARGAELECTRICA_GRUPO_H
+
+#include <string>
+
+using namespace std;
+
+class Grupo {
+  
+  public:
+    //serán constructores
+    Grupo(string, float, int);
+    Grupo();
+
+    //funciones
+    int GetCarga();
+    float GetpK();
+  
+private:
+    string nombre;
+    float pka;
+    int carga;
+
+
+};
+
+
+#endif //CARGAELECTRICA_GRUPO_H
