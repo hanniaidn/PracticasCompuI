f4cb78c8a513151ab5329678dff934409661deef
hanniaidn <113195557+hanniaidn@users.noreply.github.com>
2022-11-23 19:03:43 -0600

    Update Grupo.cpp

--- CargaElectrica/Grupo.cpp
+++ CargaElectrica/Grupo.cpp
@@ -1,1 +1,27 @@
+//
+// Created by Hannia Domínguez on 23/11/2022.
+//
 
+#include "Grupo.h"
+
+Grupo::Grupo() {
+    nombre = " ";
+    pka = 0;
+    carga = 0;
+}
+
+Grupo::Grupo(string nombreGrupo, float pkGrupo, int cargaGrupo) {
+    nombre = nombreGrupo;
+    pka = pkGrupo;
+    carga = cargaGrupo;
+}
+
+//regresa el valor de la carga de cada grupo de la molécula
+int Grupo::GetCarga() {
+    return carga;
+}
+
+//regresa el valor de pka de cada grupo de la molécula
+float Grupo::GetpK() {
+    return pka;
+}
